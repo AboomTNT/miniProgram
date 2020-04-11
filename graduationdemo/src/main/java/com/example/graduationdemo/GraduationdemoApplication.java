24448cb37b295f017558bb2ec07f3f64b9f838cb
AboomTNT <1207892282@qq.com>
2020-04-11 19:37:26 +0800

    第一次提交

---
+++ graduationdemo/src/main/java/com/example/graduationdemo/GraduationdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.graduationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduationdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraduationdemoApplication.class, args);
+    }
+
+}
---
+++ graduationdemo/src/test/java/com/example/graduationdemo/GraduationdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graduationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduationdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
